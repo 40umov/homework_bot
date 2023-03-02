e63effc2cd353f5e3b2f40a6649298b663522514
Evgeniy Sorokoumov <40umov.evg@gmail.com>
2023-03-02 22:21:10 +0300

    Delete empty lines in code.

--- homework.py
+++ homework.py
@@ -71,19 +71,15 @@ def get_api_answer(current_timestamp: int) -> dict:
         endpoint.
     """
     timestamp = current_timestamp or int(time.time())
-
     params_request = {
         'url': ENDPOINT,
         'headers': HEADERS,
         'params': {'from_date': timestamp},
     }
-
     message = ('Начало запроса к API. '
                'Запрос: {url}, {headers}, {params}.'
                ).format(**params_request)
-
     logging.info(message)
-
     try:
         response = requests.get(**params_request)
         if response.status_code != HTTPStatus.OK:
@@ -119,19 +115,14 @@ def check_response(response: dict) -> list:
         list: A list of homeworks obtained from the response.
     """
     logging.info('Проверка ответа API на корректность.')
-
     if not isinstance(response, dict):
         raise TypeError('Ответ API не является dict.')
-
     if 'homeworks' not in response or 'current_date' not in response:
         raise EmptyResponseFromAPI('Отсутствует один из ключей, homeworks или '
                                    'current_date, в ответе API.')
-
     homeworks = response.get('homeworks')
-
     if not isinstance(homeworks, list):
         raise TypeError('homeworks не является list.')
-
     return homeworks
 
 
@@ -151,17 +142,12 @@ def parse_status(homework):
         str: A string containing the status of the homework and its name.
     """
     logging.info('Проводим проверки и извлекаем статус работы.')
-
     if 'homework_name' not in homework:
         raise KeyError('Нет ключа homework_name в ответе API.')
-
     homework_name = homework.get('homework_name')
-
     homework_status = homework.get('status')
-
     if homework_status not in HOMEWORK_VERDICTS:
         raise ValueError(f'Неизвестный статус работы - {homework_status}.')
-
     return ('Изменился статус проверки работы "{homework_name}". {verdict}'
             ).format(homework_name=homework_name,
                      verdict=HOMEWORK_VERDICTS[homework_status]
@@ -218,19 +204,12 @@ def main():
         message = 'Отсутствует токен. Бот остановлен!'
         logging.critical(message)
         sys.exit(message)
-
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-
     current_timestamp = int(time.time())
-
     start_message = 'Бот начал работу.'
-
     send_message(bot, start_message)
-
     logging.info(start_message)
-
     prev_msg = ''
-
     while True:
         try:
             response = get_api_answer(current_timestamp)
